251e856fa520d1f1283b90986dfda189b20fdfd2
JishnuS420 <broburn123@gmail.com>
2023-01-23 01:29:07 -0800

    Revert "change"
    This reverts commit 78b10da6be7a052d5632eb2ce135f0386bb4f6fb.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from flask import render_template  # import render_template from "public" flask 
 from __init__ import app,db  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
-from model.scores import initScores
+from model.score import initScores
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
