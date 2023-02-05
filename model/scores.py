0015baae05d9201f066ba3e942b2af3693ed066e
chewyboba10 <evanaparri@gmail.com>
2023-02-04 19:59:14 -0800

    updating scores.py

--- model/scores.py
+++ model/scores.py
@@ -1,36 +1,27 @@
 """ database dependencies to support sqliteDB examples """
+from random import randrange
 from datetime import date
+import os, base64
 import json
 
 from __init__ import app, db
 from sqlalchemy.exc import IntegrityError
+
 
 class Score(db.Model):
     __tablename__ = 'scores'  # table name is plural, class name is singular
 
     # Define the User schema with "vars" from object
     id = db.Column(db.Integer, primary_key=True)
-    _name = db.Column(db.String(255), unique=False, nullable=False)
     _username = db.Column(db.String(255), unique=True, nullable=False)
     _score = db.Column(db.String(255), unique=False, nullable=False)
     _dos = db.Column(db.Date)
 
     # constructor of a User object, initializes the instance variables within object (self)
-    def __init__(self, name, username, score, dos=date.today()):
-        self._name = name    # variables with self prefix become part of the object, 
+    def __init__(self, username, score, dos=date.today()): # variables with self prefix become part of the object, 
         self._username = username
         self.score = score
         self._dos = dos
-
-    # a name getter method, extracts name from object
-    @property
-    def name(self):
-        return self._name
-    
-    # a setter function, allows name to be updated after initial object creation
-    @name.setter
-    def name(self, name):
-        self._name = name
     
     # a getter method, extracts email from object
     @property
@@ -78,16 +69,34 @@ class Score(db.Model):
         except IntegrityError:
             db.session.remove()
             return None
+    
+    # CRUD update: updates user name, password, phone
+    # returns self
+    def update(self, username="", score=""):
+        """only updates values with length"""
+        if len(username) > 0:
+            self.username = username
+        if len(score) > 0:
+            self.set_score(score)
+        db.session.commit()
+        return self
+
+    # CRUD delete: remove self
+    # None
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return None
 
     # CRUD read converts self to dictionary
     # returns dictionary
     def make_dict(self):
         return {
-            "name": self.name,
             "username": self.username,
             "score": self.score,
             "dos": self.dos,
         }
+
 
 
 """Database Creation and Testing """
@@ -98,11 +107,11 @@ def initScores():
     """Create database and tables"""
     db.create_all()
     """Tester data for table"""
-    u1 = Score('Alan', 'A1234l', '12', date(2023, 1, 22))
-    u2 = Score('Jishnu', 'test', '20', date(2023, 1, 21))
-    u3 = Score('Max', 'mmaxwu', '10', date(2023, 1, 20))
-    u4 = Score('Evan', 'chewyboba', '15', date(2023, 1, 19))
-    u5 = Score('Bob', 'bob123', '100', date(2023, 1, 22))
+    u1 = Score('AAA', date(2023, 1, 22))
+    u2 = Score('AAB', '20', date(2023, 1, 21))
+    u3 = Score('AAC', '10', date(2023, 1, 20))
+    u4 = Score('AAD', '15', date(2023, 1, 19))
+    u5 = Score('AAE', '100', date(2023, 1, 22))
 
     users = [u1, u2, u3, u4, u5]
 
@@ -114,4 +123,4 @@ def initScores():
             '''fails with bad or duplicate data'''
             db.session.remove()
             print(f"Records exist, duplicate email, or error: {user.uid}")
-            
+    
